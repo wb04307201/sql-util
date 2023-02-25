15b39d4951f7cf8b228e26947a69f36ac7e87c20
吴博 <wubo_aaa@163.com>
2023-02-25 08:46:57 +0800

    lombok @Data

--- src/main/java/cn/wubo/sql/util/entity/MethodEntity.java
+++ src/main/java/cn/wubo/sql/util/entity/MethodEntity.java
@@ -1,7 +1,10 @@
 package cn.wubo.sql.util.entity;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 
+@Data
 public class MethodEntity {
     //方法名称
     private String methodName;
@@ -11,51 +14,6 @@ public class MethodEntity {
     private Class[] methodParamTypes;
     //存放重载方法参数
     private ArrayList repeatMethodsParamTypes;
-
-    /**
-     * 获取参数名称
-     *
-     * @return
-     */
-    public String getMethodName() {
-        return methodName;
-    }
-
-    /**
-     * 获取方法参数类型列表
-     *
-     * @return
-     */
-    public Class[] getMethodParamTypes() {
-        return methodParamTypes;
-    }
-
-    /**
-     * 设置参数名称
-     *
-     * @param string
-     */
-    public void setMethodName(String string) {
-        methodName = string;
-    }
-
-    /**
-     * 设置参数类型列表
-     *
-     * @param classes
-     */
-    public void setMethodParamTypes(Class[] classes) {
-        methodParamTypes = classes;
-    }
-
-    /**
-     * 获取重载方法个数
-     *
-     * @return
-     */
-    public int getRepeatMethodNum() {
-        return repeatMethodNum;
-    }
 
     /**
      * 获取第i个重载方法参数列表
@@ -69,32 +27,6 @@ public class MethodEntity {
         } else {
             throw new ArrayIndexOutOfBoundsException();
         }
-    }
-
-    /**
-     * 设置重载方法个数
-     *
-     * @param i
-     */
-    public void setRepeatMethodNum(int i) {
-        repeatMethodNum = i;
-    }
-
-    /**
-     * 设置重载方法参数类型
-     *
-     * @param list
-     */
-    public void setRepeatMethodsParamTypes(ArrayList list) {
-        repeatMethodsParamTypes = list;
-    }
-
-    /*
-     *获取重载方法类型
-     * @return
-     */
-    public ArrayList getRepeatMethodsParamTypes() {
-        return repeatMethodsParamTypes;
     }
 
     /**
