56261a575b31a886d1678a9b8db2ccf63cfc06d0
wubo <wubo_aaa@163.com>
2024-02-16 08:25:22 +0800

    修复语法错误

--- src/main/java/cn/wubo/sql/util/entity/EntityUtils.java
+++ src/main/java/cn/wubo/sql/util/entity/EntityUtils.java
@@ -15,6 +15,7 @@ import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class EntityUtils {
 
@@ -54,7 +55,7 @@ public class EntityUtils {
     private static List<TableModel.ColumnModel> transToColumns(Class<?> clazz) {
         List<Field> fields = new ArrayList<>();
         getFields(clazz, fields);
-        return fields.stream().map(TableModel.ColumnModel::new).sorted(Comparator.comparing(TableModel.ColumnModel::getSort)).toList();
+        return fields.stream().map(TableModel.ColumnModel::new).sorted(Comparator.comparing(TableModel.ColumnModel::getSort)).collect(Collectors.toList());
     }
 
     /**
@@ -65,7 +66,7 @@ public class EntityUtils {
     private static void getFields(Class<?> clazz, List<Field> fields) {
         if (clazz != null) {
             // 获取指定类的所有非静态非final字段
-            fields.addAll(Arrays.stream(clazz.getDeclaredFields()).filter(field -> !field.isSynthetic()).filter(field -> !(Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers()))).toList());
+            fields.addAll(Arrays.stream(clazz.getDeclaredFields()).filter(field -> !field.isSynthetic()).filter(field -> !(Modifier.isFinal(field.getModifiers()) && Modifier.isStatic(field.getModifiers()))).collect(Collectors.toList()));
             // 递归获取父类的所有非静态非final字段
             getFields(clazz.getSuperclass(), fields);
         }
